f721e01cbf7a0bee0cf614dfc339079b1dd5d9c9
Logesh044 <formyofficial01@gmail.com>
2024-10-02 19:00:04 +0530

    add some buttons

--- script.js
+++ script.js
@@ -6,12 +6,22 @@ function clears(){
     number.value=" ";
 }
 function total(){
+    let display=document.getElementById('number').value;
+    if(display.includes('^')){
+        let parts=display.split('^')
+        let base=parseFloat(parts[0])
+        let exponent=parseFloat(parts[1])
+        var power=Math.pow(base,exponent)
+        number.value = power
+    }
+    else{
     try{
         number.value=eval(number.value)
     }
     catch(error){
         number.value= "Error";
     }
+}
 }
 function backspace(){
     number.value=number.value.slice(0,-1)
